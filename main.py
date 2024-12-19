d38bf50d1590596fff8fa44a42b2d1eace3bac03
Crazy-Jester <gena.xd.1994@gmail.com>
2024-12-19 19:52:42 +0300

    Практическое задание по теме "Переменные"

---
+++ main.py
@@ -0,0 +1,10 @@
+
+the_number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+print( 'Курс',course_name,'всего задач', the_number_of_completed_DZ,'затрачено чесов',
+       number_of_hours_spent, 'среднее время выполнения', number_of_hours_spent / the_number_of_completed_DZ)
+
+
+
+
